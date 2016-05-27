73fb60181049566705f8c109f79344733ea56b1c
Aleks Real <aleksanderreal@gmail.com>
2016-05-27 23:25:11 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firehosepreteamquiz::Application.config.secret_key_base = '800671d242241d3f7ed0995d08aef91cf2b088e04e87bde0ffdb66e38c8163ac122c0ad3c3ea0e451468dccb34a6b538f52552bdada9640e9025d86c27e336d6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Firehosepreteamquiz::Application.config.session_store :cookie_store, key: '_Firehosepreteamquiz_session'
